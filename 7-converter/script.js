44bdc46abc772970279e1d7bb913b35cd7f14452
Deadmanch <stepanova.nata.r@gmail.com>
2023-12-04 10:38:16 +0300

    Вынес в отдельные переменные

--- 7-converter/script.js
+++ 7-converter/script.js
@@ -16,20 +16,21 @@ const converter = (sum, currency, targetCurrency) => {
 			const EUR_TO_USD = 1.09;
 			const USD_TO_EUR = 0.916755;
 
-		
+		const currencyToLowerCase = currency.toLowerCase();
+		const targetCurrencyToLowerCase = targetCurrency.toLowerCase();
 
 			switch(true) {
-				case currency.toLowerCase() === 'руб' && targetCurrency.toLowerCase() === 'usd':
+				case currencyToLowerCase === 'руб' && targetCurrencyToLowerCase === 'usd':
 					return sum * RUB_TO_USD
-				case currency.toLowerCase() === 'usd' && targetCurrency.toLowerCase() === 'руб' :
+				case currencyToLowerCase === 'usd' && targetCurrencyToLowerCase === 'руб' :
 					return sum * USD_TO_RUB
-				case currency.toLowerCase() === 'руб' && targetCurrency.toLowerCase() === 'eur':
+				case currencyToLowerCase === 'руб' && targetCurrencyToLowerCase === 'eur':
 					return sum * RUB_TO_EUR
-				case currency.toLowerCase() === 'eur' && targetCurrency.toLowerCase() === 'руб' :
+				case currencyToLowerCase === 'eur' && targetCurrencyToLowerCase === 'руб' :
 					return sum * EUR_TO_RUB
-				case currency.toLowerCase() === 'usd' && targetCurrency.toLowerCase() === 'eur':
+				case currencyToLowerCase === 'usd' && targetCurrencyToLowerCase === 'eur':
 					return sum * USD_TO_EUR
-				case currency.toLowerCase() === 'eur' && targetCurrency.toLowerCase() === 'usd' :
+				case currencyToLowerCase === 'eur' && targetCurrencyToLowerCase === 'usd' :
 					return sum * EUR_TO_USD
 				default:
 					return null
