446a62ce8e5ec5a822d294973ea2881a2cb64722
Deadmanch <stepanova.nata.r@gmail.com>
2023-12-05 10:26:05 +0300

    Выполнил задание №8

---
+++ 8-crypto/script.js
@@ -0,0 +1,14 @@
+function crypto(password) {
+	const passwordReverse = password.split('').reverse(); 
+	const firstCharPassword = passwordReverse.splice(0, 3);
+	const cryptoPassword = passwordReverse.concat(firstCharPassword);
+	return cryptoPassword.join('');
+}
+
+function check(cryptoPassword, password) {
+	const checkPassword = crypto(password);
+	return cryptoPassword === checkPassword ? true : false;
+}
+console.log(crypto('123654789'));
+
+console.log(check('456321987', '123654789'));
